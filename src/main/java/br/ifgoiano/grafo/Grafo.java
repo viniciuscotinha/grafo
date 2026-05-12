42f2db3bb91e0998b41f33f576811b29eb004bd7
vinicius <viniciuscotinha@gmail.com>
2026-05-12 20:05:10 -0300

    Refactor Grafo class to support adding and updating vertices and edges; enhance tests for new functionality

--- src/main/java/br/ifgoiano/grafo/Grafo.java
+++ src/main/java/br/ifgoiano/grafo/Grafo.java
@@ -12,21 +12,32 @@ public class Grafo {
         this.Vertices = new ArrayList<Vertice>();
     }
 
-    public Vertice adicionarVertice(long id, String texto) {
+    public Vertice adicionarOuAtualizarVertice(long id, String texto) {
         Vertice vertice = this.pegarVertice(id);
         if(vertice == null) {
             vertice = new Vertice(id, texto);
             Vertices.add(vertice);
             return vertice;
         } else {
+            vertice.texto = texto;
             return vertice;
         }
     }
 
-    public void adicionarAresta(Vertice origem, Vertice destino, int peso, String texto) {
-        Arestas.add(new Aresta(origem, destino, peso, texto));
+    public Aresta adicionarOuAtualizarAresta(Vertice origem, Vertice destino, int peso, String texto) {
+        Aresta aresta = this.pegarAresta(origem, destino);
+        if (aresta == null){
+            aresta = new Aresta(origem, destino, peso, texto);
+            Arestas.add(aresta);
+            return aresta;
+        } else {
+            aresta.peso = peso;
+            aresta.texto = texto;
+            return aresta;
+        }
     }
 
+    
     public int contarLacos() {
         return 0;
     }
@@ -34,7 +45,7 @@ public class Grafo {
     public boolean ehCompleto() {
         return false;
     }
-
+    
     public int grauVertice() {
         return 0;
     }
@@ -51,6 +62,14 @@ public class Grafo {
         for(int i = 0; i < Vertices.size(); i++) {
             if (id == Vertices.get(i).id) {
                 return Vertices.get(i);
+            }
+        }
+        return null;
+    }
+    private Aresta pegarAresta(Vertice origem, Vertice destino) {
+        for (int i = 0; i < Arestas.size(); i++) {
+            if (origem == Arestas.get(i).origem && destino == Arestas.get(i).destino) {
+                return Arestas.get(i);
             }
         }
         return null;
--- src/test/java/br/ifgoiano/grafo/GrafoTest.java
+++ src/test/java/br/ifgoiano/grafo/GrafoTest.java
@@ -2,6 +2,7 @@ package br.ifgoiano.grafo;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,8 +15,49 @@ class GrafoTest {
     @DisplayName("Adicionar um vertice")
     void deveAdicionarUmVertice() {
         Grafo g = new Grafo();
-        g.adicionarVertice(1, "texto1");
+        g.adicionarOuAtualizarVertice(1, "texto1");
         assertEquals(g.Vertices.get(0).id, 1);
     }
 
+    @Test
+    @DisplayName("Adicionar um vertice e adicionar outro de mesmo id")
+    void deveAdicionarMesmoVertice() {
+        Grafo g = new Grafo();
+        Vertice v1 = g.adicionarOuAtualizarVertice(1, "texto1");
+        Vertice v2 = g.adicionarOuAtualizarVertice(1, "texto2");
+        assertAll(
+            () -> assertEquals(v1, v2),
+            () -> assertEquals("texto2", v1.texto)
+        );
+    }
+    @Test
+    @DisplayName("Adicionar uma aresta")
+    void deveAdicionarUmAresta() {
+        Grafo g = new Grafo();
+        Vertice v1 = g.adicionarOuAtualizarVertice(1, "texto1");
+        Vertice v2 = g.adicionarOuAtualizarVertice(2, "texto2");
+        Aresta a1 = g.adicionarOuAtualizarAresta(v1, v2, 5, "texto1");
+        assertAll(
+            () -> assertEquals(a1, g.Arestas.get(0)),
+            () -> assertEquals(v1, g.Arestas.get(0).origem),
+            () -> assertEquals(v2, g.Arestas.get(0).destino),
+            () -> assertEquals(5, g.Arestas.get(0).peso),
+            () -> assertEquals("texto1", g.Arestas.get(0).texto)
+        );
+    }
+
+    @Test
+    @DisplayName("Adicionar uma aresta e adicionar outro de mesmo jeito")
+    void deveAdicionarMesmoAresta() {
+        Grafo g = new Grafo();
+        Vertice v1 = g.adicionarOuAtualizarVertice(1, "texto1");
+        Vertice v2 = g.adicionarOuAtualizarVertice(2, "texto2");
+        Aresta a1 = g.adicionarOuAtualizarAresta(v1, v2, 5, "texto1");
+        Aresta a2 = g.adicionarOuAtualizarAresta(v1, v2, 6, "texto2");
+        assertAll(
+            () -> assertEquals(a1, a2),
+            () -> assertEquals(6, g.Arestas.get(0).peso),
+            () -> assertEquals("texto2", g.Arestas.get(0).texto)
+        );
+    }
 }
